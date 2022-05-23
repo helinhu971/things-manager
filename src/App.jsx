c8aaf4b8ba66e55f30695f04291f165ddefdfa56
helinhu971 <hcrodrigues.filho@gmail.com>
2022-05-23 15:41:37 -0300

    fix: create loginPage

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/loginPage';
+
+function App() {
+  return (
+    <div className='app'>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/loginPage/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("submit", { email, password });
+  };
+
+  return (
+    <div id="login">
+      <h1 className="title">Login do sistema</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="field">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="password">Senha</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <div className="actions">
+          <button type="submit">Entrar</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
